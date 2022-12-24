4d599a87cba2529d8a59271e360df2895bf431e6
urbankreatives <117651600+urbankreatives@users.noreply.github.com>
2022-12-24 08:31:41 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,47 @@ router.get('/proj',function(req,res){
     res.render('projects')
 })
 
+router.get('/serv',function(req,res){
+    res.render('services')
+})
+
+
 router.get('/cont',function(req,res){
     res.render('contact')
 })
+
+router.get('/serv',function(req,res){
+    res.render('services')
+})
+
+router.get('/dione',function(req,res){
+    res.render('team')
+})
+
+router.get('/emily',function(req,res){
+    res.render('team1')
+})
+
+router.get('/joseph',function(req,res){
+    res.render('team2')
+})
+
+
+router.get('/melody',function(req,res){
+    res.render('team3')
+})
+
+router.get('/paul',function(req,res){
+    res.render('team4')
+})
+
+
+router.get('/phinias',function(req,res){
+    res.render('team5')
+})
+
+
+
 
 
 
